68fc99e4c1fe11a2064a2735ffce41b7397844db
Sairam-77 <sairam.babu@kprinfo.com>
2024-01-26 23:21:17 +0530

    all

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { MantineProvider } from '@mantine/core'
+import '@mantine/core/styles.css';
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  // <React.StrictMode>
+  <MantineProvider defaultColorScheme="light">
+    <App />
+  </MantineProvider>
+  // </React.StrictMode>,
+)
